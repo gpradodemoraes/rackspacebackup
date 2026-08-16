ba97650741a7a42ee6412bcf081fdb371d9c1c20
Gabriel Prado de Moraes <gpradodemoraes@outlook.com>
2026-08-16 19:24:51 -0300

    somente acessando o rackspace se o arquivo de accessa não existe

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using json = nlohmann::json;
 #include "git_hash.h"
 #include "remote.hpp"
 #include "config.hpp"
+#include <string>
 extern const char *build_date;
 extern const char *rackspace_access_filename;
 
@@ -27,14 +28,27 @@ int main() {
 	rackspaceconfig::config conf = data.get<rackspaceconfig::config>();
 
 	fmt::println("CONFIG: username {}; api_key {}", conf.get_username(), conf.get_api_key());
-	std::string response;
-	if (authenticate_to_rackspace_cloud(&conf, response, error) > 0) {
-		fmt::println("Erro chamando authenticate_to_rackspace_cloud: {}", error);
+
+	f.open(rackspaceconfig::rackspace_access_filename);
+	int tries = 3;
+	while (!f.good() && tries-- > 0) {
+		std::string response;
+		if (authenticate_to_rackspace_cloud(&conf, response, error) > 0) {
+			fmt::println("Erro chamando authenticate_to_rackspace_cloud: {}", error);
+			return 1;
+		}
+
+		std::ofstream access_filename(rackspaceconfig::rackspace_access_filename);
+		access_filename << response;
+		access_filename.close();
+		f.open(rackspaceconfig::rackspace_access_filename);
+	}
+
+	if (!f.good()) {
+		fmt::println("Giving up on file {}", rackspaceconfig::rackspace_access_filename);
 		return 1;
 	}
 
-	std::ofstream access_filename(rackspaceconfig::rackspace_access_filename);
-	access_filename << response;
-	access_filename.close();
+	f.close();
 	return 0;
 }
