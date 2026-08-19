e1b01b2baf2b624db729b7a44b0e05dcdab0e75a
Gabriel Prado de Moraes <gpradodemoraes@outlook.com>
2026-08-19 13:20:58 -0300

    mudando o nome do handler do arquivo para ficar menos confuso

--- main.cpp
+++ main.cpp
@@ -53,23 +53,24 @@ int main() {
 
 	char error[1024] = { 0 };
 
-	std::ifstream f(rackspaceconfig::rackspace_config_filename);
-	if (!f.good()) {
+	std::ifstream config_file_handler(rackspaceconfig::rackspace_config_filename);
+	if (!config_file_handler.good()) {
 		fmt::println("Could not open {}", rackspaceconfig::rackspace_config_filename);
 		return 1;
 	}
-	json data = json::parse(f);
-	f.close();
+	json data = json::parse(config_file_handler);
+	config_file_handler.close();
 	rackspaceconfig::config conf = data.get<rackspaceconfig::config>();
 
 	fmt::println("CONFIG: username {}; api_key {}", conf.get_username(), conf.get_api_key());
 
-	f.open(rackspaceconfig::rackspace_access_filename);
+	std::ifstream access_file_handler;
+	access_file_handler.open(rackspaceconfig::rackspace_access_filename);
 	bool is_expired = true;
 	int is_expired_tries = 3;
-	while (is_expired&&is_expired_tries-- > 0) {
+	while (is_expired && is_expired_tries-- > 0) {
 		int tries = 3;
-		while (!f.good() && tries-- > 0) {
+		while (!access_file_handler.good() && tries-- > 0) {
 			std::string response;
 			if (authenticate_to_rackspace_cloud(&conf, response, error) > 0) {
 				fmt::println("Erro chamando authenticate_to_rackspace_cloud: {}", error);
@@ -79,16 +80,16 @@ int main() {
 			std::ofstream access_filename(rackspaceconfig::rackspace_access_filename);
 			access_filename << response;
 			access_filename.close();
-			f.open(rackspaceconfig::rackspace_access_filename);
+			access_file_handler.open(rackspaceconfig::rackspace_access_filename);
 		}
 
-		if (!f.good()) {
+		if (!access_file_handler.good()) {
 			fmt::println("Giving up on file {}", rackspaceconfig::rackspace_access_filename);
 			return 1;
 		}
 
-		json access_json_data = json::parse(f);
-		f.close();
+		json access_json_data = json::parse(access_file_handler);
+		access_file_handler.close();
 
 		std::string access_token = access_json_data["access"]["token"]["id"].get<std::string>();
 		std::string expires = access_json_data["access"]["token"]["expires"].get<std::string>();
