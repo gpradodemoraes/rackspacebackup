b6f131186ffc45cf310977b222c70fa0cdc0acfb
Gabriel Prado de Moraes <gpradodemoraes@outlook.com>
2026-08-21 16:19:03 -0300

    criando a lista de arquivos que serão baixados e depois deletados. Apenas os arquivos que existem a mais de 24 horas.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <fmt/core.h>
 #include <fstream>
 #include <nlohmann/json.hpp>
+#include <regex>
+#include <chrono>
+#include <string>
+#include <iomanip>
+#include <sstream>
 #include "compiled_with.h"
 #include "git_hash.h"
 #include "config.hpp"
 #include "rackspace.hpp"
+
 extern const char *build_date;
 extern const char *rackspace_access_filename;
 
@@ -15,6 +21,20 @@ int main() {
 	fmt::println("Build Time: {}", build_date);
 
 	char error[1024] = { 0 };
+
+	auto in_the_past = std::chrono::system_clock::now() - std::chrono::hours(24);
+	std::time_t time = std::chrono::system_clock::to_time_t(in_the_past);
+
+	std::tm tm_local;
+#if defined(_WIN32)
+	localtime_s(&tm_local, &time);
+#else
+	localtime_r(&time, &tm_local);
+#endif
+
+	std::ostringstream oss;
+	oss << std::put_time(&tm_local, "%Y-%m-%d-%H-%M-%S");
+	std::string twenty_four_hours_ago = oss.str();
 
 	std::ifstream config_file_handler(rackspaceconfig::rackspace_config_filename);
 	if (!config_file_handler.good()) {
@@ -41,6 +61,16 @@ int main() {
 	if (get_rackspace_container_list_of_files(&cloudfiles_info, std::string("sqlitebackup"), &file_list, error) > 0) {
 		fmt::println("Erro chamando get_container_list_of_files: {}", error);
 		return 1;
+	}
+
+	std::regex get_file_date(R"(^([^.]*)\.([^.]*))");
+	for (const auto &file : file_list) {
+		std::smatch match;
+		if (std::regex_search(file, match, get_file_date)) {
+			if (match[2].str() < twenty_four_hours_ago) {
+				fmt::println("we will download and then delete this file: {}<==>{}", match[1].str(), match[2].str());
+			}
+		}
 	}
 
 	return 0;
